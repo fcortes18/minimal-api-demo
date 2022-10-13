6311dc8d99a14fe62eb6c383e7d95b8e45cc8d19
Francisco Cortés <ing.cortesrf.94@gmail.com>
2022-10-13 00:28:40 -0500

    reset api in main to get ready for live demo.

---
+++ tests/MinimalAPI.IntegrationTests/HelloWorldTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace MinimalAPI.IntegrationTests
+{
+    public class HelloWorldTests
+    {
+        [Fact]
+        public async Task TestRootEndpoint()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            var response = await client.GetAsync("/");
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var responseText = await response.Content.ReadAsStringAsync();
+            Assert.False(string.IsNullOrEmpty(responseText));
+            Assert.Equal("Hello world!", responseText);
+        }
+    }
+}
